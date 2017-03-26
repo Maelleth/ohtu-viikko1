e48b4b156f6bb1617cd90d7ebdc401366a5cce13
Maethorr <tolvanen.sara@hotmail.com>
2017-03-26 22:01:36 +0300

    verkkokauppa

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,41 +1,45 @@
 package ohtu.verkkokauppa;
 
-public class Kauppa {
+public class Kauppa implements KauppaIO {
 
-    private Varasto varasto;
-    private Pankki pankki;
+    private VarastoIO varasto;
+    private PankkiIO pankki;
     private Ostoskori ostoskori;
-    private Viitegeneraattori viitegeneraattori;
+    private ViitegeneraattoriIO viitegeneraattori;
     private String kaupanTili;
 
-    public Kauppa() {
-        varasto = Varasto.getInstance();
-        pankki = Pankki.getInstance();
-        viitegeneraattori = Viitegeneraattori.getInstance();
+    public Kauppa(Varasto varasto, Pankki pankki, Viitegeneraattori viitegeneraattori) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
         kaupanTili = "33333-44455";
     }
 
+    @Override
     public void aloitaAsiointi() {
         ostoskori = new Ostoskori();
     }
 
+    @Override
     public void poistaKorista(int id) {
-        Tuote t = varasto.haeTuote(id); 
+        Tuote t = varasto.haeTuote(id);
         varasto.palautaVarastoon(t);
     }
 
+    @Override
     public void lisaaKoriin(int id) {
-        if (varasto.saldo(id)>0) {
-            Tuote t = varasto.haeTuote(id);             
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
             ostoskori.lisaa(t);
             varasto.otaVarastosta(t);
         }
     }
 
+    @Override
     public boolean tilimaksu(String nimi, String tiliNumero) {
         int viite = viitegeneraattori.uusi();
         int summa = ostoskori.hinta();
-        
+
         return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
     }
 
