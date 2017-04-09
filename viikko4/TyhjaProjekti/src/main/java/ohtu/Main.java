6affaa06395683edbafdc5f5112656b49db98976
Maethorr <tolvanen.sara@hotmail.com>
2017-04-09 21:04:57 +0300

    tehtävät 1-3

--- viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -9,23 +9,71 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
         String studentNr = "012345678";
-        if ( args.length>0) {
+        if (args.length > 0) {
             studentNr = args[0];
         }
 
-        String url = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+        Course course = getCourse(1);
+        Submission[] submissions = getSubmissions(studentNr);
 
+        printOutput(course, submissions);
+
+    }
+
+    private static Course getCourse(int courseID) throws IOException {
+        String url = "https://ohtustats2017.herokuapp.com/courses/" + courseID + ".json";
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
+        Gson mapper = new Gson();
+        return mapper.fromJson(bodyText, Course.class);
+    }
+
+    private static Submission[] getSubmissions(String studentNro) throws IOException {
+        String url = "http://ohtustats2017.herokuapp.com/students/" + studentNro + "/submissions";
+        String bodyText = Request.Get(url).execute().returnContent().asString();
 
         Gson mapper = new Gson();
-        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
-        
-        System.out.println("Oliot:");
-        for (Submission submission : subs) {
-            System.out.println(submission);
+        return mapper.fromJson(bodyText, Submission[].class);
+    }
+
+    private static void printOutput(Course course, Submission[] submissions) {
+        System.out.println("Kurssi: " + course.name + ", " + course.term + "\n");
+        System.out.println("opiskelijanumero: " + submissions[0].student_number + "\n");
+
+        int totalDone = 0;
+        int totalHours = 0;
+        for (Submission s : submissions) {
+            System.out.print(String.format(
+                    "viikko %d: tehtyja tehtavia yhteensa: %d (maksimi %d) "
+                    + "aikaa kului %d tuntia, tehdyt tehtavat: ",
+                    s.week, s.tehdytTehtävät().length, course.exercises(s.week),
+                    s.hours));
+
+            for (Integer done : s.tehdytTehtävät()) {
+                System.out.print(done + " ");
+            }
+
+            totalDone += s.tehdytTehtävät().length;
+            totalHours += s.hours;
+            System.out.println("");
+        }
+
+        System.out.println("\n");
+        System.out.println("yhteensa: " + totalDone + " tehtavaa " + totalHours + " tuntia");
+    }
+
+    public static class Course {
+
+        public String term;
+        public String name;
+        public int week1, week2, week3, week4, week5, week6;
+
+        public int exercises(int week) {
+            if (week < 0 || week > 6) {
+                return 0;
+            }
+            int[] weeks = {week1, week2, week3, week4, week5, week6};
+            return weeks[week - 1];
         }
 
     }
--- viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko4/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,19 +1,32 @@
 package ohtu;
 
+import java.util.ArrayList;
+
 public class Submission {
-    private String student_number;
 
-    public String getStudent_number() {
-        return student_number;
+    public String student_number;
+    public int week;
+    public int hours;
+    public int id;
+    public String comments;
+
+    public boolean a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+            a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21;
+
+    public Integer[] tehdytTehtävät() {
+        boolean[] exercises = {
+            a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12,
+            a13, a14, a15, a16, a17, a18, a19, a20, a21
+        };
+
+        ArrayList<Integer> done = new ArrayList<>();
+        for (int i = 0; i < exercises.length; i++) {
+            if (exercises[i]) {
+                done.add(i + 1);
+            }
+        }
+
+        return (Integer[]) done.toArray(new Integer[done.size()]);
     }
 
-    public void setStudent_number(String student_number) {
-        this.student_number = student_number;
-    }
-
-    @Override
-    public String toString() {
-        return student_number;
-    }
-    
 }
